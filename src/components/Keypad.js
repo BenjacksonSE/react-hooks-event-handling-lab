8fb465cea763bdafca34386a321ee6e16b0769c5
Ben Jackson <benjacksonSE@gmail.com>
2021-11-01 10:25:04 -0400

    All tests passed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
 // Code Keypad Component Here
+import React from "react"
+
+function Keypad() {
+    function keysBeingEntered() {
+        console.log("Entering password...")
+    }
+    
+    
+    return (
+        <input
+            type="password"
+            name="keypad"
+            onChange={keysBeingEntered}
+            placeholder="secret password"
+        />
+    )
+    
+}
+
+export default Keypad
